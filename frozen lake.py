d97811978524e21ac44d9b95c3819c88b6271729
ShangliXu <sx616@nyu.edu>
2022-04-17 20:39:59 -0700

    Add files via upload

---
+++ frozen lake.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 17 15:18:13 2022
+
+@author: passi
+"""
+
+import hiive.mdptoolbox.example
+from hiive.mdptoolbox import mdp
+from gym.envs.toy_text.frozen_lake import generate_random_map
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+def init():
+    np.random.seed(0)
+    random_map = generate_random_map(size=5, p=0.8)
+    P, R = hiive.mdptoolbox.example.openai("FrozenLake-v1", desc=random_map)
+    # R.shape  (100, 4), P.shape  (4, 100, 100)
+    # PP, RR = P.copy(), R.copy()
+    return P, R
+
+# gamma = 0.5
+for gamma in [0.9, 0.5]:
+    
+    policy = pd.DataFrame()
+    iters = pd.DataFrame(columns = ['iters'])
+    times = pd.DataFrame(columns = ['time'])
+    mean_discrepancy = []
+    
+    print('ValueIteration')
+    epsilons = [0.01, 0.001]
+    for epsilon_ in epsilons:
+        P, R = init()
+        vi = mdp.ValueIteration(P, R, gamma, epsilon = epsilon_)
+        vi.run()
+        policy['vi' + str(epsilon_)] = vi.policy
+        iters.loc['vi' + str(epsilon_)] = vi.iter
+        times.loc['vi' + str(epsilon_)] = vi.time
+    
+    
+    print('PolicyIteration')
+    P, R = init()
+    pi = mdp.PolicyIteration(P, R, gamma)
+    pi.run()
+    policy['pi'] = vi.policy
+    iters.loc['pi'] = vi.iter
+    times.loc['pi'] = vi.time
+    
+    
+    print('QLearning')
+    n_iters = [10000, 100000]
+    for n_iter_ in n_iters:
+        P, R = init()
+        ql = mdp.QLearning(P, R, gamma, n_iter = n_iter_)
+        ql.run()
+        policy['ql' + str(n_iter_)] = ql.policy
+        times.loc['ql' + str(n_iter_)] = ql.time
+        # ql_df['Q'+ str(n_iter_)] = ql.Q
+        mean_discrepancy.append(ql.mean_discrepancy)
+        plt.plot(ql.mean_discrepancy)
+        plt.xlabel('number of every 100 iterations')
+        plt.ylabel('Vector of learned value discrepancy mean')
+        plt.title('Q learning with n_iter = ' + str(n_iter_))
+        plt.show()
+
